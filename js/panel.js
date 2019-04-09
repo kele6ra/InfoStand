40af2a306cb78fea2f2ed6aa0b9ee1ac90ec46cd
Andrey Ovchinnikov <ovchinnikov@strim-tech.com>
2019-04-09 17:11:58 +0300

    added page navigation buttons, fixes

--- js/panel.js
+++ js/panel.js
@@ -2,10 +2,11 @@ class Panel {
     constructor() {
         this.page = new Home();
         this.addNavigationMenu();
+        this.addPageNavigationButtons();
         this.prevPage = "Home";
         this.openBoundFunction = this.openPage.bind(this);
-        this.buttons = document.querySelectorAll('.as2-navigation__button');
-        this.setButtonsEvents();
+
+
         setTimeout(() => {
             this.checkPageSize();
         }, 200);
@@ -18,12 +19,46 @@ class Panel {
         NavigationMenu += '<button class="as2-navigation__button as2-navigation__button_small"><img class ="as2-navigation__buttonImg" src="img/expand_more.svg"></img></button>';
         NavigationMenu += '</div>';
         NavigationMenu += '<div class="as2-navigation__column">';
-        NavigationMenu += '<button class="as2-navigation__button"><img class ="as2-navigation__buttonImg" src="img/menu.svg"></img></button>';
+        NavigationMenu += '<button class="as2-navigation__button"><img class ="as2-navigation__buttonImg" src="img/apps.svg"></img></button>';
         NavigationMenu += '</div>';
         NavigationMenu += '<div class="as2-navigation__column">';
         NavigationMenu += '<button class="as2-navigation__button"><img class ="as2-navigation__buttonImg" src="img/arrow_back.svg"></img></button>';
         NavigationMenu += '</div></div>';
         $('.as2-wrap__navigation').append(NavigationMenu);
+        this.navigationButtons = document.querySelectorAll('.as2-navigation__button');
+        this.navigationButtons[0].addEventListener("click", (e) => {
+            this.scrollPage('toTop');
+        });
+        this.navigationButtons[1].addEventListener("click", (e) => {
+            this.scrollPage('toBottom');
+        });
+        this.navigationButtons[2].addEventListener("click", (e) => {
+            this.openPage('Apps');
+        });
+        this.navigationButtons[3].addEventListener("click", (e) => {
+            this.openPrevPage();
+        });
+    }
+
+    addPageNavigationButtons() {
+        this.pageElement = document.querySelector('.as2-wrap__page');
+        let NavigationButtons = '<div class="as2-button as2-button__pageNavigation as2-button__pageNavigation_prev"><img src="img/left-chevron.svg"></img></div>';
+        NavigationButtons += '<div class="as2-button as2-button__pageNavigation as2-button__pageNavigation_next"><img src="img/right-chevron.svg"></img></div>';
+        $('.as2-wrap__page').append(NavigationButtons);
+        this.pageButtons = document.querySelectorAll('.as2-button__pageNavigation');
+        this.makePageNavigationVisible(false);
+        this.pageButtons[0].addEventListener("click", (e) => {
+            this.changePage("prev");
+        });
+        this.pageButtons[1].addEventListener("click", (e) => {
+            this.changePage("next");
+        });
+        this.pageElement.addEventListener("mouseover", (e) => {
+            this.makePageNavigationVisible(true);
+        });
+        this.pageElement.addEventListener("mouseout", (e) => {
+            this.makePageNavigationVisible(false);
+        });
     }
 
     checkPageSize() {
@@ -32,6 +67,14 @@ class Panel {
             $('.as2-navigation__scrollButtons').css('visibility', 'visible');
         } else {
             $('.as2-navigation__scrollButtons').css('visibility', 'hidden');
+        }
+    }
+
+    makePageNavigationVisible(visible) {
+        if (visible) {
+            $('.as2-button__pageNavigation').css('visibility', 'visible');
+        } else {
+            $('.as2-button__pageNavigation').css('visibility', 'hidden');
         }
     }
 
@@ -56,12 +99,16 @@ class Panel {
         switch (newPage) {
             case "Home":
                 this.page = new Home();
+                this.addPageNavigationButtons();
+                this.makePageNavigationVisible(false);
                 break;
             case "Apps":
                 this.page = new Apps(this.openBoundFunction);
                 break;
             case "Forecast":
                 this.page = new Forecast();
+                this.addPageNavigationButtons();
+                this.makePageNavigationVisible(false);
                 break;
             default:
                 break;
@@ -72,22 +119,33 @@ class Panel {
         }, 200);
     }
 
-    openPrevPage() {
-        this.openPage(this.prevPage)
+    changePage(pageButton) {
+        if (pageButton == "next") {
+            switch (this.page.name) {
+                case "Home":
+                    this.openPage("Forecast");
+                    break;
+                case "Forecast":
+                    this.openPage("Home");
+                    break;
+                default:
+                    break;
+            }
+        } else if (pageButton == "prev") {
+            switch (this.page.name) {
+                case "Home":
+                    this.openPage("Forecast");
+                    break;
+                case "Forecast":
+                    this.openPage("Home");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    setButtonsEvents() {
-        this.buttons[0].addEventListener("click", (e) => {
-            this.scrollPage('toTop');
-        });
-        this.buttons[1].addEventListener("click", (e) => {
-            this.scrollPage('toBottom');
-        });
-        this.buttons[2].addEventListener("click", (e) => {
-            this.openPage('Apps');
-        });
-        this.buttons[3].addEventListener("click", (e) => {
-            this.openPrevPage();
-        });
+    openPrevPage() {
+        this.openPage(this.prevPage)
     }
 }
